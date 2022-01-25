c3e4c17a8d2660ad1284abe5bf13fffffaa65b4b
sieunju <j.sieun73@gmail.com>
2022-01-25 23:27:52 +0900

    feat: node 테스트 서버 수정

--- node-server/router/index.js
+++ node-server/router/index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const router = express.Router()
 const auth = require('./auth')
 const goods = require('./goods')
+const jsend = require('./jsend')
 
-router.use('/',auth)
-router.use('/',goods)
+router.use('/', auth)
+router.use('/', goods)
+router.use('/', jsend)
 
 
 module.exports = router
