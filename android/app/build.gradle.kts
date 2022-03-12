7efd7b5d05c1d5fa8e7a65259b44e5429ceb9263
sieunju <j.sieun73@gmail.com>
2022-03-12 21:53:48 +0900

    fix: 잘못된 부분 원복 처리

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,14 +7,6 @@ plugins {
 }
 
 android {
-    signingConfigs {
-        create("release") {
-            storeFile = file("/Users/juhongmin/Documents/workStudio/tilkeystore/tilkeystore")
-            storePassword = "apple1234!"
-            keyAlias = "til"
-            keyPassword = "apple1234!"
-        }
-    }
     compileSdk = Apps.compileSdkVersion
     buildToolsVersion = Apps.buildToolsVersion
 
@@ -31,19 +23,18 @@ android {
     }
 
     buildTypes {
-        debug {
+        getByName("debug") {
             isMinifyEnabled = false
         }
 
-        release {
+        getByName("release") {
             isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
-            signingConfig = signingConfigs.getByName("release")
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
         jvmTarget = "11"
