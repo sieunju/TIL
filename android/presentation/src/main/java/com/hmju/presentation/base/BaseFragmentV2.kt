89cdc72be3dee29bf09801fc3e7b306e562e5b54
sieunju <j.sieun73@gmail.com>
2022-03-29 23:27:03 +0900

    feat: 베이스 클래스들 리펙토링 기본적인부분 추가

---
+++ android/presentation/src/main/java/com/hmju/presentation/base/BaseFragmentV2.kt
@@ -0,0 +1,9 @@
+package com.hmju.presentation.base
+
+/**
+ * Description :
+ *
+ * Created by juhongmin on 2022/03/19
+ */
+class BaseFragmentV2 {
+}
---
+++ android/presentation/src/main/java/com/hmju/presentation/refactor_base/RefactorTestViewModel.kt
@@ -0,0 +1,13 @@
+package com.hmju.presentation.refactor_base
+
+import com.hmju.presentation.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+/**
+ * Description :
+ *
+ * Created by juhongmin on 2022/03/19
+ */
+@HiltViewModel
+class RefactorTestViewModel : BaseViewModel() {
+}
