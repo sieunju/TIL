3e561c642dd35568bfcc6a5d3e9b9e02bf73390d
sieunju <j.sieun73@gmail.com>
2022-04-20 19:02:40 +0900

    feat: base 모듈 추가

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -10,8 +10,8 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.1.3")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:1.6.10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.20")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.6.20")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
     }
 }
