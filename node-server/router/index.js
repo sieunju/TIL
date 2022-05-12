c8e48290a721e56156cb3b689eb6b2ae0558d581
sieunju <j.sieun73@gmail.com>
2022-05-12 21:32:50 +0900

    feat: Rx 에서 에러 헨들링 관련 TIL 를 하기위한 서버 셋팅 - 간단히 이정도면 되겠지??

--- node-server/router/index.js
+++ node-server/router/index.js
@@ -3,10 +3,12 @@ const router = express.Router()
 const auth = require('./auth')
 const goods = require('./goods')
 const jsend = require('./jsend')
+const errorhandling = require('./errorhandling')
 
 router.use('/', auth)
 router.use('/', goods)
 router.use('/', jsend)
+router.use('/', errorhandling)
 
 
 module.exports = router
