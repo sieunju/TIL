821310bf31fa8d0014609f0c49c139d64cd6b23f
sieunju <j.sieun73@gmail.com>
2022-06-14 23:28:28 +0900

    remove: 기존에 불필요하게 남아있던 모듈 삭제

--- android/tracking/src/main/java/com/til/tracking/models/TrackingBodyUiModel.kt
+++
@@ -1,7 +0,0 @@
-package com.til.tracking.models
-
-import com.til.tracking.R
-
-internal data class TrackingBodyUiModel(
-    val body : String = ""
-) : BaseTrackingUiModel(R.layout.vh_tracking_body)
